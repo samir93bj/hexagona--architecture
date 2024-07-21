79b394b6201395923a8231bd52636e221ec16ea8
samir93bj <samir93bj@gmail.com>
2024-07-21 16:57:19 -0300

    refactor: Remove unused AppModule

--- src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PaymentModule } from './contexts/payments/repository/payment.module';
-
-@Module({
-  imports: [PaymentModule],
-  controllers: [],
-  providers: [],
-})
-export class AppModule {}
