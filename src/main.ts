d5c16185bc3047c62430eee4b7d31ed57ba34a0e
samir93bj <samir93bj@gmail.com>
2024-07-21 01:59:15 -0300

    refactor: Update main.ts to use FastifyAdapter and add global filters and interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,36 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 
-import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
+import { AppModule } from './app/app.module';
+import { API } from './app/http-api/routes/routes-constants';
+import { NestLoggerService } from './contexts/shared/logger/infraestructura/nestjs.logger-service';
+import { ErrorResponseNormalizerFilter } from './app/http-api/response-normalizer/error-response-normalizer.filter';
+import { SuccessResponseNormalizerInterceptor } from './app/http-api/response-normalizer/success-response-normalizer.interceptor';
+
+import { Logger } from './contexts/shared/logger/domain';
+import { LoggerInterceptor } from './contexts/shared/logger/infraestructura/logger.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+    { bufferLogs: true },
+  );
+
+  app.useLogger(app.get(NestLoggerService));
+  app.setGlobalPrefix(API);
+
+  app.useGlobalFilters(app.get(ErrorResponseNormalizerFilter));
+  app.useGlobalInterceptors(
+    app.get(LoggerInterceptor),
+    app.get(SuccessResponseNormalizerInterceptor),
+  );
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -14,7 +40,13 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get<string>('PORT', '3000');
+  const logger = app.get(Logger);
+
+  await app.listen(port, '0.0.0.0');
+
+  logger.info(`App is ready and listening on port ${port} 🚀`);
 }
 bootstrap();
 
